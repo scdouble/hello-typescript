13dd3eaf27e6fb8fcf712c1e2e86107899c38d72
chen.a.sheng <chen.a.sheng@accenture.com>
2022-01-11 23:22:44 +0900

    class basic

---
+++ part4-class/dist/01_class_basic.js
@@ -0,0 +1,31 @@
+"use strict";
+// classを使ってクラスを定義する
+/**
+ * クラスの中には属性とメソッドがある
+ */
+class Person {
+    constructor() {
+        // インスタンス属性を定義できる
+        // ここの属性はInstanceのみで確認できる。newしたあとしか見れない
+        // readonly は読み取り専用の属性にする
+        this.name = "son go kuu"; //read onlyにする
+        this.ang = 18;
+    }
+    // クラスのメソッドを定義
+    sayHello() {
+        console.log("Hello");
+    }
+    //メソッドがStaticが付いているのなら、クラスからInstance生成しないで利用できる
+    static sayHello() {
+        console.log("Hello");
+    }
+}
+// staticを使うとクラス属性を定義できる
+Person.age = 19;
+const per = new Person();
+console.log(per);
+console.log(per.name); // クラスのインスタンスからアクセスする
+console.log(Person.age); // Instanceを作らず直接クラスからアクセスできる
+// per.name = "tom"; //読み取り専用で変更できない
+per.sayHello();
+Person.sayHello();
