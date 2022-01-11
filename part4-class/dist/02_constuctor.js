78c0537bc141fb4e8969e35df3d2f7328b2d338d
chen.a.sheng <chen.a.sheng@accenture.com>
2022-01-11 23:40:10 +0900

    constructor

---
+++ part4-class/dist/02_constuctor.js
@@ -0,0 +1,23 @@
+"use strict";
+class Dog {
+    // コンストラクタはInstance生成時に実行される
+    constructor(name, age) {
+        // インスタンスメソッドの中ではthisは現在の自分自身のオブジェクトを指す
+        // コンストラクタの中で、thisを使って属性を付与できる
+        // console.log(this);
+        this.name = name;
+        this.age = age;
+    }
+    bark() {
+        // alert("wan wan");
+        // メソッドの中でthisは実行元のオブジェクトを取得できる
+        console.log(this);
+        console.log(this.name);
+    }
+}
+const dog = new Dog("hei", 3);
+const dog2 = new Dog("bai", 3);
+console.log(dog);
+console.log(dog2);
+dog.bark();
+dog2.bark();
