353b4dcb9315e6a4b095207c3369304614e06f2e
chen.a.sheng <chen.a.sheng@accenture.com>
2022-01-12 19:47:26 +0900

    add score panel

--- part5-snake/src/index.ts
+++ part5-snake/src/index.ts
@@ -1,36 +1,6 @@
 import "./style/index.less";
+import Food from "./modules/Food";
+import ScorePanel from "./modules/ScorePanel";
 
-//餌のクラス定義
-class Food {
-  //　htmlエレメント表す属性
-  element: HTMLElement;
-  constructor() {
-    // webページの中のエレメントを取得する
-    this.element = document.getElementById("food")!;
-  }
-  // 餌のｘ軸のメソッド
-  get X() {
-    return this.element.offsetLeft;
-  }
-  get Y() {
-    return this.element.offsetTop;
-  }
-
-  // 餌の位置を変更するメソッド
-  change() {
-    // ランダム位置を生成
-    // 餌の位置の最小は0, 最大は290
-    // 蛇は一回の移動で10px進むので、餌の位置も10pxごとで置かなければいけない
-    let top = Math.round(Math.random() * 29) * 10;
-    let left  = Math.round(Math.random() * 29) * 10;
-    
-    this.element.style.top = top + 'px';
-    this.element.style.left = left + 'px'
-  }
-}
-
-// test
-const food = new Food();
-console.log(food.X, food.Y);
+const food = new Food()
 food.change();
-console.log(food.X, food.Y);
---
+++ part5-snake/src/modules/ScorePanel.ts
@@ -0,0 +1,49 @@
+// スコアパネル
+class ScorePanel {
+  // score levelは点数とレベルを記録
+  score = 0;
+  level = 1;
+
+  // 点数とレベルのHTML要素
+  scoreEle: HTMLElement;
+  levelEle: HTMLElement;
+
+  // レベルの制限
+  maxLevel: number;
+  // レベル上げの点数を設定
+  upScore: number;
+
+  constructor(maxLevel: number = 10, upScore: number = 10) {
+    this.scoreEle = document.getElementById("score")!;
+    this.levelEle = document.getElementById("level")!;
+    this.maxLevel = maxLevel;
+    this.upScore = upScore;
+  }
+
+  // scoreを足す方法
+  addScore() {
+    // scoreを増加
+    this.score++;
+    this.scoreEle.innerHTML = this.score + "";
+    // 点数の条件を判断
+    if (this.score % this.upScore === 0) {
+      this.levelUp();
+    }
+  }
+
+  levelUp() {
+    // レベルの上限を設定
+    if (this.level < this.maxLevel) {
+      this.levelEle.innerHTML = ++this.level + "";
+    }
+  }
+}
+
+//   // test
+//   const sp = new ScorePanel(10,2);
+
+//   for (let i = 0; i < 100; i++) {
+//     sp.addScore();
+//   }
+
+export default ScorePanel;
